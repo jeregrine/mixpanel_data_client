74320d934dd2dc1395c173cdfdf9be040b9646e7
Holger Amann <holger@sauspiel.de>
2015-07-16 19:00:31 +0200

    use ssl endpoint

--- lib/mixpanel_data_client.ex
+++ lib/mixpanel_data_client.ex
@@ -1,6 +1,6 @@
 defmodule MixpanelDataClient do
   use HTTPoison.Base
-  @base_url "http://mixpanel.com/api/2.0/"
+  @base_url "https://mixpanel.com/api/2.0/"
 
   @doc """
     Fetch data from the mixpanel data api.
